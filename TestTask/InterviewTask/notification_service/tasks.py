bda421966788d22498eab4b882d72ff3f3fcacde
DNS <ivanov2161@gmail.com>
2022-08-23 13:45:04 +0500

    optimized code

--- TestTask/InterviewTask/notification_service/tasks.py
+++ TestTask/InterviewTask/notification_service/tasks.py
@@ -9,7 +9,6 @@ import os
 URL = os.getenv('URL')
 TOKEN = os.getenv('TOKEN')
 
-
 header = {
     'Authorization': f'Bearer {TOKEN}',
     'Content-Type': 'application/json'}
@@ -36,15 +35,14 @@ def send_message(mailing_id, client_id):
 @shared_task(name='check_mailings')
 def check_mailings():
     mailings = Mailing.objects.all()
-    clients = Client.objects.all()
     timenow = datetime.now(timezone)
     for mailing in mailings:
         mail = Message.objects.filter(mailings_id=mailing.pk).all()
         amountmessages = mail.count()
         if amountmessages == 0:
+            clients = Client.objects.filter(operatorcode=mailing.operatorcode, tag=mailing.tag)
             for client in clients:
-                if client.operatorcode == mailing.operatorcode and client.tag == mailing.tag:
-                    if mailing.starttime < timenow < mailing.endtime:
+                if mailing.starttime < timenow < mailing.endtime:
                         send_message(mailing.pk, client.pk)
         else:
             pass
